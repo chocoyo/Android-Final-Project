e6c3cdd9264ca6975ecc1b5fd44a13acc2111530
Mike Hodges <mikehodges7@outlook.com>
2020-11-14 21:42:19 -0600

    Added Basic Toolbar

--- app/src/main/java/com/mhodges/finalproject/MainActivity.java
+++ app/src/main/java/com/mhodges/finalproject/MainActivity.java
@@ -1,8 +1,14 @@
 package com.mhodges.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //to inflate the xml menu file
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+
+
+    //to handle events
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        boolean returnVal = false;
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                Log.d("JG", "refresh menu item");
+                returnVal = true;
+                break;
+            case R.id.action_settings:
+                Log.d("JG", "settings menu item");
+                returnVal = true;
+                break;
+        }
+        return returnVal;
     }
 }
