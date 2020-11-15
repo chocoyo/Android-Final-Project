98c5c5ed519788332a758dc65fdd9066f9a5c446
Mike Hodges <mikehodges7@outlook.com>
2020-11-14 22:10:01 -0600

    Added Basic Item Model

---
+++ app/src/main/java/com/mhodges/finalproject/Item.java
@@ -0,0 +1,13 @@
+package com.mhodges.finalproject;
+
+public class Item {
+    private String name;
+    private double price;
+    private String description;
+    private String seller;
+
+
+    private String link;
+
+
+}
