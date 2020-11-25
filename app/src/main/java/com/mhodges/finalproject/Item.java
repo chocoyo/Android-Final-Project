c55c6735c46ca0c2d494654dacdeab4ae79601fb
Mike Hodges <mikehodges7@outlook.com>
2020-11-24 21:52:31 -0600

    Added Base Classes To Allow Items To Be Displayed

--- app/src/main/java/com/mhodges/finalproject/Item.java
+++ app/src/main/java/com/mhodges/finalproject/Item.java
@@ -4,10 +4,38 @@ public class Item {
     private String name;
     private double price;
     private String description;
-    private String seller;
-
-
     private String link;
 
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
---
+++ app/src/main/java/com/mhodges/finalproject/ListOfItemAdapter.java
@@ -0,0 +1,58 @@
+package com.mhodges.finalproject;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListOfItemAdapter extends RecyclerView.Adapter<ListOfItemAdapter.ViewHolder> {
+
+    List<Item> items;
+    LayoutInflater layoutInflater;
+
+
+    public ListOfItemAdapter(Context context, List<Item> items){
+        layoutInflater = LayoutInflater.from(context);
+        this.items = items;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.list_as_item_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(items.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.txtTitle);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("MH", "onClick: " + items.get(getAdapterPosition()).getName());
+                }
+            });
+        }
+    }
+}
